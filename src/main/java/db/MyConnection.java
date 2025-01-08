7709e99995db6d2cb5efe725baa4f501d75a7ace
laxmibagodi <laxmibagodi25@gmail.com>
2025-01-09 00:09:28 +0530

    connection successful with database

--- src/main/java/db/MyConnection.java
+++ src/main/java/db/MyConnection.java
@@ -1,4 +1,34 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyConnection {
+    public static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost3306/filehider?useSSL=false", "root", "srush@212556");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection successfull");
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args){
+
+        MyConnection.getConnection();
+    }
 }
